cd51a19287fbf680fada687453d41c3d6a675003
mooncrite-lgtm <mooncrite@gmail.com>
2025-12-30 22:55:06 +0100

    chore: setup prisma and database

--- src/app.js
+++ src/app.js
@@ -9,3 +9,10 @@ app.get("/health", (req, res) => {
 })
 
 export default app
+
+import { prisma } from "./config/db.js"
+
+app.get("/db-test", async (req, res) => {
+  const users = await prisma.user.findMany()
+  res.json(users)
+})
